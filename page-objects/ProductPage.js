dc135352c72ab5757c15077190c6343051005d89
Joseph <jlawrence106@gmail.com>
2025-05-21 00:36:23 +1200

    script changes

--- page-objects/ProductPage.js
+++ page-objects/ProductPage.js
@@ -2,7 +2,26 @@ class ProductPage{
 
     constructor (page) {
 
-        
+        this.itemname = page.locator('[data-test=inventory-item-name]');
+        this.itemdesc = page.locator('[data-test=inventory-item-desc]');
+        this.itemprice = page.locator('[inventory-item-price]');
+        this.addtocart = page.locator('#add-to-cart');
+        this.remove = page.locator('#remove');
+        this.carticon = page.locator('#.shopping_cart_badge');
+
+    }
+
+    async validatepageDetails() {
+
+    }
+
+    async addtocart() {
+
+    }
+
+    async navigatetoCart() {
 
     }
 }
+
+module.exports = {ProductPage}
